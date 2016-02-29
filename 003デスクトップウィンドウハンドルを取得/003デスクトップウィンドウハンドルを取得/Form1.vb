df41b48d130bdda0435b60ec3090f4124c1d171b
kenoga dev <dev.kenoga.owg@gmail.com>
2016-02-29 22:26:38 +0900

    デスクトップのウィンドウハンドル取得

---
+++ 003デスクトップウィンドウハンドルを取得/003デスクトップウィンドウハンドルを取得/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function GetDesktopWindow() As IntPtr
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = GetDesktopWindow().ToString()
+    End Sub
+
+End Class
