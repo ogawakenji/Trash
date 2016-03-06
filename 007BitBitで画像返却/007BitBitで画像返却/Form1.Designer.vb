3f85ed259feaac0c8787413bb78b17846e0e31db
kenoga dev <dev.kenoga.owg@gmail.com>
2016-03-06 23:10:16 +0900

    007追加

---
+++ 007BitBitで画像返却/007BitBitで画像返却/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("ＭＳ ゴシック", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 33)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "●"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("ＭＳ ゴシック", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(620, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 33)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "▲"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("ＭＳ ゴシック", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(67, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(209, 37)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(18, 45)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(651, 375)
+        Me.Panel1.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(407, 220)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(681, 432)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ 007BitBitで画像返却/007BitBitで画像返却/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    ''' <summary>
+    ''' WindowFormPoint 指定された座標を含むウィンドウのハンドルを取得 の宣言
+    ''' </summary>
+    ''' <param name="lpPoint"></param>
+    ''' <returns></returns>
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function WindowFromPoint(ByVal lpPoint As Point) _
+                                            As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function GetParent(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SendMessage(ByVal hwnd As IntPtr, ByVal msg As UInteger, ByVal mParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SetForegroundWindow(hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+
+    End Function
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        '' カーソルを十字に変更
+        Me.Cursor = Cursors.Cross
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        '' カーソルをデフォルトに戻す
+        Me.Cursor = Cursors.Default
+
+        '' マウス座標よりハンドル取得
+        Dim hwnd As IntPtr
+        hwnd = WindowFromPoint(Cursor.Position)
+
+        Me.TextBox1.Text = hwnd.ToString()
+    End Sub
+
+
+    Const SRCCOPY As Integer = 13369376
+    Const CAPTUREBLT As Integer = 1073741824
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetDC(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("gdi32.dll")>
+    Private Shared Function BitBlt(ByVal hDestDC As IntPtr,
+        ByVal x As Integer, ByVal y As Integer,
+        ByVal nWidth As Integer, ByVal nHeight As Integer,
+        ByVal hSrcDC As IntPtr,
+        ByVal xSrc As Integer, ByVal ySrc As Integer,
+        ByVal dwRop As Integer) As Integer
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function ReleaseDC(ByVal hwnd As IntPtr,
+        ByVal hdc As IntPtr) As IntPtr
+    End Function
+
+    ''' <summary>
+    ''' プライマリスクリーンの画像を取得する
+    ''' </summary>
+    ''' <returns>プライマリスクリーンの画像</returns>
+    Public Shared Function CaptureScreen() As Bitmap
+        'プライマリモニタのデバイスコンテキストを取得
+        Dim disDC As IntPtr = GetDC(IntPtr.Zero)
+        'Bitmapの作成
+        Dim bmp As New Bitmap(Screen.PrimaryScreen.Bounds.Width,
+            Screen.PrimaryScreen.Bounds.Height)
+        'Graphicsの作成
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        'Graphicsのデバイスコンテキストを取得
+        Dim hDC As IntPtr = g.GetHdc()
+        'Bitmapに画像をコピーする
+        BitBlt(hDC, 0, 0, bmp.Width, bmp.Height, disDC, 0, 0, SRCCOPY)
+        '解放
+        g.ReleaseHdc(hDC)
+        g.Dispose()
+        ReleaseDC(IntPtr.Zero, disDC)
+
+        Return bmp
+    End Function
+
+    <StructLayout(LayoutKind.Sequential)>
+    Private Structure RECT
+        Public left As Integer
+        Public top As Integer
+        Public right As Integer
+        Public bottom As Integer
+    End Structure
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetWindowDC(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetForegroundWindow() As IntPtr
+    End Function
+
+    <DllImport("user32.dll")>
+    Private Shared Function GetWindowRect(ByVal hwnd As IntPtr,
+        ByRef lpRect As RECT) As Integer
+    End Function
+
+    ''' <summary>
+    ''' アクティブなウィンドウの画像を取得する
+    ''' </summary>
+    ''' <returns>アクティブなウィンドウの画像</returns>
+    Public Shared Function CaptureActiveWindow() As Bitmap
+        'アクティブなウィンドウのデバイスコンテキストを取得
+        Dim hWnd As IntPtr = GetForegroundWindow()
+        Dim winDC As IntPtr = GetWindowDC(hWnd)
+        'ウィンドウの大きさを取得
+        Dim winRect As New RECT
+        GetWindowRect(hWnd, winRect)
+        'Bitmapの作成
+        Dim bmp As New Bitmap(winRect.right - winRect.left,
+            winRect.bottom - winRect.top)
+        'Graphicsの作成
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        'Graphicsのデバイスコンテキストを取得
+        Dim hDC As IntPtr = g.GetHdc()
+        'Bitmapに画像をコピーする
+        BitBlt(hDC, 0, 0, bmp.Width, bmp.Height, winDC, 0, 0, SRCCOPY)
+        '解放
+        g.ReleaseHdc(hDC)
+        g.Dispose()
+        ReleaseDC(hWnd, winDC)
+
+        Return bmp
+    End Function
+
+    Public Shared Function CapturehWnd(hWnd As IntPtr) As Bitmap
+        ' 指定ウィンドウハンドルの画像を取得
+        Dim winDC As IntPtr = GetWindowDC(hWnd)
+        'ウィンドウの大きさを取得
+        Dim winRect As New RECT
+        GetWindowRect(hWnd, winRect)
+        'Bitmapの作成
+        Dim bmp As New Bitmap(winRect.right - winRect.left,
+            winRect.bottom - winRect.top)
+        'Graphicsの作成
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        'Graphicsのデバイスコンテキストを取得
+        Dim hDC As IntPtr = g.GetHdc()
+        'Bitmapに画像をコピーする
+        BitBlt(hDC, 0, 0, bmp.Width, bmp.Height, winDC, 0, 0, SRCCOPY)
+        '解放
+        g.ReleaseHdc(hDC)
+        g.Dispose()
+        ReleaseDC(hWnd, winDC)
+
+        Return bmp
+    End Function
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim bmp As Bitmap = CapturehWnd(CType(Me.TextBox1.Text, IntPtr))
+
+        Me.PictureBox1.Image = bmp
+
+
+    End Sub
+End Class
