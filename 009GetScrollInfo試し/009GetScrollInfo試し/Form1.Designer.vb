0f003499a94232c612caae1f25c1b9a4c417dd09
kenoga dev <dev.kenoga.owg@gmail.com>
2016-04-04 00:36:17 +0900

    コミット

---
+++ 009GetScrollInfo試し/009GetScrollInfo試し/Form1.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("ＭＳ ゴシック", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(75, 18)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 40)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 40)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "●"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label2.Location = New System.Drawing.Point(411, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 40)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "■"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label3.Location = New System.Drawing.Point(241, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 40)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "▲"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label4.Location = New System.Drawing.Point(304, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(57, 40)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "△"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("ＭＳ ゴシック", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(19, 83)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(449, 73)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Location = New System.Drawing.Point(500, 193)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 100)
+        Me.Panel1.TabIndex = 5
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label7.Location = New System.Drawing.Point(13, 214)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(37, 40)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "3"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label6.Location = New System.Drawing.Point(15, 119)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(37, 40)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "2"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("ＭＳ ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.Label5.Location = New System.Drawing.Point(14, 11)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 40)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "1"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(509, 60)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(100, 96)
+        Me.RichTextBox1.TabIndex = 6
+        Me.RichTextBox1.Text = "1" & Global.Microsoft.VisualBasic.ChrW(10) & "2" & Global.Microsoft.VisualBasic.ChrW(10) & "3" & Global.Microsoft.VisualBasic.ChrW(10) & "4" & Global.Microsoft.VisualBasic.ChrW(10) & "5" & Global.Microsoft.VisualBasic.ChrW(10) & "6" & Global.Microsoft.VisualBasic.ChrW(10) & "7" & Global.Microsoft.VisualBasic.ChrW(10) & "8" & Global.Microsoft.VisualBasic.ChrW(10) & "9" & Global.Microsoft.VisualBasic.ChrW(10) & "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoScroll = True
+        Me.ClientSize = New System.Drawing.Size(780, 168)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ 009GetScrollInfo試し/009GetScrollInfo試し/Form1.vb
@@ -0,0 +1,195 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    ''' <summary>
+    ''' WindowFormPoint 指定された座標を含むウィンドウのハンドルを取得 の宣言
+    ''' </summary>
+    ''' <param name="lpPoint"></param>
+    ''' <returns></returns>
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function WindowFromPoint(ByVal lpPoint As Point) _
+                                            As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function GetParent(ByVal hwnd As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SendMessage(ByVal hwnd As IntPtr, ByVal msg As UInteger, ByVal mParam As IntPtr, ByVal lParam As IntPtr) As IntPtr
+
+    End Function
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function SetForegroundWindow(hWnd As IntPtr) As <MarshalAs(UnmanagedType.Bool)> Boolean
+
+    End Function
+
+    Const WM_VSCROLL As Integer = &H115I
+
+    Const WM_PAINT As Integer = &HFI
+
+    Const WM_SETREDRAW As Integer = &HBI
+
+
+
+    Const SB_LINEUP As Short = &H0S
+
+    Const SB_LINELEFT As Short = &H0S
+
+    Const SB_LINEDOWN As Short = &H1S
+
+    Const SB_LINERIGHT As Short = &H1S
+
+    Const SB_PAGEUP As Short = &H2S
+
+    Const SB_PAGELEFT As Short = &H2S
+
+    Const SB_PAGEDOWN As Short = &H3S
+
+    Const SB_PAGERIGHT As Short = &H3S
+
+    Const SB_THUMBPOSITION As Short = &H4S
+
+    Const SB_THUMBTRACK As Short = &H5S
+
+    Const SB_TOP As Short = &H6S
+
+    Const SB_LEFT As Short = &H6S
+
+    Const SB_BOTTOM As Short = &H7S
+
+    Const SB_RIGHT As Short = &H7S
+
+    Const SB_ENDSCROLL As Short = &H8S
+
+
+
+    <StructLayout(LayoutKind.Sequential)> Public Structure SCROLLINFO
+
+        Public cbSize As Integer
+
+        Public fMask As Integer
+
+        Public nMin As Integer
+
+        Public nMax As Integer
+
+        Public nPage As Integer
+
+        Public nPos As Integer
+
+        Public nTrackPos As Integer
+
+    End Structure
+
+
+
+    Private Const SB_HORZ = 0    '標準水平スクロールバーを指定する
+
+    Private Const SB_VERT = 1     '標準垂直スクロールバーを指定する
+
+    Private Const SB_CTL = 2     'スクロールバーコントロールを指定する
+
+
+
+    Private Const SIF_RANGE = &H1
+
+    Private Const SIF_PAGE = &H2
+
+    Private Const SIF_POS = &H4
+
+    Private Const SIF_DISABLENOSCROLL = &H8
+
+    Private Const SIF_TRACKPOS = &H10
+
+    Private Const SIF_ALL = (SIF_RANGE Or SIF_PAGE Or SIF_POS Or SIF_TRACKPOS)
+
+
+
+    <DllImport("user32.dll", CharSet:=CharSet.Auto)>
+    Private Shared Function GetScrollInfo(ByVal hWnd As IntPtr,
+                                        ByVal nBar As Integer,
+        <MarshalAs(UnmanagedType.Struct)> ByRef lpScrollInfo As SCROLLINFO) As Integer
+    End Function
+
+
+
+    <DllImport("user32.dll")>
+    Private Shared Function SetScrollInfo(ByVal hWnd As IntPtr,
+                                        ByVal nBar As Integer,
+        <MarshalAs(UnmanagedType.Struct)> ByRef lpScrollInfo As SCROLLINFO) As Integer
+    End Function
+
+    'Private Const WM_VSCROLL As Integer = &H115
+    Private Const WM_MOUSEWHEEL As Integer = &H020A
+
+
+    Private Sub Label1_MouseDown(sender As Object, e As MouseEventArgs) Handles Label1.MouseDown
+        '' カーソルを十字に変更
+        Me.Cursor = Cursors.Cross
+    End Sub
+
+    Private Sub Label1_MouseUp(sender As Object, e As MouseEventArgs) Handles Label1.MouseUp
+        '' カーソルをデフォルトに戻す
+        Me.Cursor = Cursors.Default
+
+        '' マウス座標よりハンドル取得
+        Dim hwnd As IntPtr
+        hwnd = WindowFromPoint(Cursor.Position)
+
+        Me.TextBox1.Text = hwnd.ToString()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+        ' 1000ミリ秒スリープ
+        System.Threading.Thread.Sleep(1000)
+
+        Dim hWnd As IntPtr = CType(Me.TextBox1.Text, IntPtr)
+        SetForegroundWindow(hWnd)
+
+        ' 1000ミリ秒スリープ
+        System.Threading.Thread.Sleep(1000)
+
+        Dim si As SCROLLINFO
+        si.cbSize = Len(si)
+        si.fMask = ScrollInfoMask.SIF_ALL
+
+        GetScrollInfo(CType(Me.TextBox1.Text, IntPtr), 1, si)
+
+        Me.TextBox2.Text = si.nPos.ToString
+
+
+        'SendMessage(CType(Me.TextBox1.Text, IntPtr), WM_VSCROLL, CType(7, IntPtr), CType(0, IntPtr))
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        SendMessage(CType(Me.TextBox1.Text, IntPtr), WM_MOUSEWHEEL, CType(-1000, IntPtr), CType(0, IntPtr))
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Public Enum ScrollInfoMask As Integer
+        SIF_RANGE = &H1
+        SIF_PAGE = &H2
+        SIF_POS = &H4
+        SIF_DISABLENOSCROLL = &H8
+        SIF_TRACKPOS = &H10
+        SIF_ALL = (SIF_RANGE Or SIF_PAGE Or SIF_POS Or SIF_TRACKPOS)
+    End Enum
+
+    Private Sub RichTextBox1_VScroll(sender As Object, e As EventArgs) Handles RichTextBox1.VScroll
+        Dim si As SCROLLINFO
+        si.cbSize = Len(si)
+        si.fMask = ScrollInfoMask.SIF_ALL
+        Dim lRet As Integer = GetScrollInfo(RichTextBox1.Handle, 1, si)
+        If si.nPage + si.nPos = si.nMax Then
+            MessageBox.Show("最終スクロール")
+        End If
+
+    End Sub
+End Class
