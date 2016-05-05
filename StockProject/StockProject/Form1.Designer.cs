a5f8eded3eacb8884fa14353c80215a8436415dd
kenoga dev <dev.kenoga.owg@gmail.com>
2016-05-06 00:28:30 +0900

    追加

---
+++ StockProject/StockProject/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace StockProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(831, 455);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(857, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 77);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GetHtml";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(831, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "http://info.finance.yahoo.co.jp/ranking/?kd=8&tm=d&vl=a&mk=1&p=1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(857, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "sgmlReader";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(857, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 77);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "parseHtml";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 523);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ StockProject/StockProject/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Sgml;
+using System.Xml.Linq;
+
+namespace StockProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int getHoge()
+        {
+            return 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Utility.HtmlUtil htmlUtil = new Utility.HtmlUtil();
+            this.textBox1.Text = htmlUtil.GetHtml(this.textBox2.Text);
+
+
+         
+
+
+
+
+        }
+
+
+        static async Task<string> GetWebPageAsync(Uri uri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // ユーザーエージェント文字列をセット（オプション）
+                client.DefaultRequestHeaders.Add(
+                    "User-Agent",
+                    "Mozilla/5.0 (Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko");
+
+                // 受け入れ言語をセット（オプション）
+                client.DefaultRequestHeaders.Add("Accept-Language", "ja-JP");
+
+                // タイムアウトをセット（オプション）
+                client.Timeout = TimeSpan.FromSeconds(10.0);
+
+                try
+                {
+                    // Webページを取得するのは、事実上この1行だけ
+                    return await client.GetStringAsync(uri);
+                }
+                catch (HttpRequestException e)
+                {
+                    // 404エラーや、名前解決失敗など
+                    Console.WriteLine("\n例外発生!");
+                    // InnerExceptionも含めて、再帰的に例外メッセージを表示する
+                    Exception ex = e;
+                    while (ex != null)
+                    {
+                        Console.WriteLine("例外メッセージ: {0} ", ex.Message);
+                        ex = ex.InnerException;
+                    }
+                }
+                catch (TaskCanceledException e)
+                {
+                    // タスクがキャンセルされたとき（一般的にタイムアウト）
+                    Console.WriteLine("\nタイムアウト!");
+                    Console.WriteLine("例外メッセージ: {0} ", e.Message);
+                }
+                return null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XDocument xml;
+            using (var sgml = new SgmlReader() { Href = this.textBox2.Text ,IgnoreDtd = true  })
+            {
+                xml = XDocument.Load(sgml); // たった3行でHtml to Xml
+            }
+
+            var ns = xml.Root.Name.Namespace;
+            var query = xml.Descendants(ns + "table")
+                .Last()
+                .Descendants(ns + "tr")
+                .Skip(1) // テーブル一行目は項目説明なので飛ばす
+                .Select(el => el.Elements(ns + "td").ToList())
+                .Select(es => new
+                {
+                    Title = es.First().Value,
+                    ReleaseDate = es.Last().Value
+                });
+
+            // 書き出し
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Title + " - " + item.ReleaseDate);
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            XDocument xml;
+
+            try
+            {
+                Utility.HtmlUtil htmlUtil = new Utility.HtmlUtil();
+                xml = htmlUtil.ParseHtml(this.textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            var ns = xml.Root.Name.Namespace;
+
+            var q =
+                from s in xml.Descendants(ns + "tr")
+                select s;
+
+            foreach (var s in q )
+            {
+                //Console.WriteLine(s.Value );
+                foreach (var s2 in s.Descendants())
+                {
+                    Console.WriteLine(s2.Value );
+                }
+
+            }
+
+
+
+
+
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            const int WM_KEYDOWN = 0x100;
+            var keyCode = (Keys)(msg.WParam.ToInt32() &
+                                  Convert.ToInt32(Keys.KeyCode));
+            if ((msg.Msg == WM_KEYDOWN && keyCode == Keys.A)
+                && (ModifierKeys == Keys.Control)
+                && textBox1.Focused)
+            {
+                textBox1.SelectAll();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
+    }
+
+
+
+}
