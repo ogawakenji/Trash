a133018eb906a430d1230b67c73199306c0f13a7
kenoga dev <dev.kenoga.owg@gmail.com>
2016-07-01 00:21:45 +0900

    追加

---
+++ 011Winium.Cruciatus試し/011Winium.Cruciatus試し/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _011Winium.Cruciatus試し
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Winium.Cruciatus";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 75);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Winium.WebDriver";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 152);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Winium.Cruciatus";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 011Winium.Cruciatus試し/011Winium.Cruciatus試し/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Winium.Cruciatus.Core;
+using Winium.Cruciatus.Extensions;
+using System.Windows.Automation;
+using OpenQA.Selenium.Winium;
+
+
+
+namespace _011Winium.Cruciatus試し
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var calc = new Winium.Cruciatus.Application(@"C:\dev\svn\trunk\Trash\Trash\011Winium.Cruciatus試し\TestForm\bin\Debug\TestForm.exe");
+            //calc.Start();
+            //var winFinder = By.Name("Form1").AndType(ControlType.Window);
+            //var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+
+
+            ////電卓の'5','6','7'ボタンクリック
+            ////AutomationId指定
+            ////win.FindElementByName("button1").Click();
+
+            //win.FindElementByUid("button1").Click();
+            //win.FindElementByUid("button2").Click();
+            //win.FindElementByUid("button3").Click();
+
+            ////Console.Write("Press any key to continue . . . ");
+            ////Console.ReadKey(true);
+
+            var calc = new Winium.Cruciatus.Application("C:/windows/system32/calc.exe");
+            calc.Start();
+
+            var winFinder = By.Name("電卓").AndType(ControlType.Window);
+            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+            //var menu = win.FindElementByUid("Menubar").ToMenu();
+
+            //menu.SelectItem("関数電卓");
+            //menu.SelectItem("履歴");
+
+            win.FindElementByUid("132").Click(); // 2
+            //win.FindElementByUid("93").Click(); // +
+            //win.FindElementByUid("134").Click(); // 4
+            //win.FindElementByUid("97").Click(); // ^
+            //win.FindElementByUid("138").Click(); // 8
+            //win.FindElementByUid("121").Click(); // =
+
+            //calc.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var options = new DesktopOptions();
+            options.ApplicationPath = @"C:\Windows\System32\notepad.exe";
+            //Winium.Desktop.Driver.exeの場所指定
+            WiniumDriver driver = new WiniumDriver(@"C:\dev\svn\trunk\Trash\Trash\011Winium.Cruciatus試し\Winium.Desktop.Driver", options);
+            driver.FindElementByClassName("Edit").SendKeys("Hello World.");
+
+
+
+
+        }
+    }
+}
