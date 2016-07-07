ffd459a94180df43a7a2127e614b4bd8e9fa2781
kenoga dev <dev.kenoga.owg@gmail.com>
2016-07-08 08:08:19 +0900

    Winium.Cruciatusうまくうごかない

--- 011Winium.Cruciatus試し/011Winium.Cruciatus試し/Form1.cs
+++ 011Winium.Cruciatus試し/011Winium.Cruciatus試し/Form1.cs
@@ -42,24 +42,38 @@ namespace _011Winium.Cruciatus試し
             ////Console.Write("Press any key to continue . . . ");
             ////Console.ReadKey(true);
 
-            var calc = new Winium.Cruciatus.Application("C:/windows/system32/calc.exe");
-            calc.Start();
+            //var calc = new Winium.Cruciatus.Application("C:/windows/system32/calc.exe");
+            //calc.Start();
 
-            var winFinder = By.Name("電卓").AndType(ControlType.Window);
-            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
-            //var menu = win.FindElementByUid("Menubar").ToMenu();
+            //var winFinder = By.Name("電卓").AndType(ControlType.Window);
+            //var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+            ////var menu = win.FindElementByUid("Menubar").ToMenu();
 
-            //menu.SelectItem("関数電卓");
-            //menu.SelectItem("履歴");
+            ////menu.SelectItem("関数電卓");
+            ////menu.SelectItem("履歴");
 
-            win.FindElementByUid("132").Click(); // 2
-            //win.FindElementByUid("93").Click(); // +
-            //win.FindElementByUid("134").Click(); // 4
-            //win.FindElementByUid("97").Click(); // ^
-            //win.FindElementByUid("138").Click(); // 8
-            //win.FindElementByUid("121").Click(); // =
+            //win.FindElementByUid("132").Click(); // 2
+            //                                     //win.FindElementByUid("93").Click(); // +
+            //                                     //win.FindElementByUid("134").Click(); // 4
+            //                                     //win.FindElementByUid("97").Click(); // ^
+            //                                     //win.FindElementByUid("138").Click(); // 8
+            //                                     //win.FindElementByUid("121").Click(); // =
 
             //calc.Close();
+
+            var calc = new Winium.Cruciatus.Application(@"C:\Windows\System32\calc.exe");
+            calc.Start();
+            var winFinder = By.Name("電卓").AndType(ControlType.Window);
+            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+
+            //電卓の'5','6','7'ボタンクリック
+            //AutomationId指定
+            win.FindElementByUid("num5Button").Click();
+            win.FindElementByUid("num6Button").Click();
+            win.FindElementByUid("num7Button").Click();
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
 
         }
 
---
+++ 011Winium.Cruciatus試し/TestConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using Winium.Cruciatus.Core;
+using Winium.Cruciatus.Extensions;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Winium.Cruciatus.Application(@"C:\Windows\System32\calc.exe");
+            calc.Start();
+            var winFinder = By.Name("電卓").AndType(ControlType.Window);
+            var win = Winium.Cruciatus.CruciatusFactory.Root.FindElement(winFinder);
+
+            //電卓の'5','6','7'ボタンクリック
+            //AutomationId指定
+            win.FindElementByUid("num5Button").Click();
+            win.FindElementByUid("num6Button").Click();
+            win.FindElementByUid("num7Button").Click();
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
